7698a0812017248eba03aa360be93def54f70000
digitaladopshub-maker <digital.adops.hub@gmail.com>
2026-09-19 00:48:16 +0500

    Update fonts.py

--- fonts.py
+++ fonts.py
@@ -1,54 +1,40 @@
 import os
 from PIL import ImageFont
 
+def get_font_family(font_choice):
+    # Dropdown choice ko clean font identifier mein map karna
+    font_map = {
+        "AntonioZull-Brush": "AntonioZull-Brush",
+        "BebasNeue-Regular": "BebasNeue-Regular",
+        "Impact Club": "Impact Club",
+        "Impact": "Impact",
+        "Impactbrutas": "Impactbrutas",
+        "IMPACTED": "IMPACTED",
+        "Impacted2.0": "Impacted2.0",
+        "Impact-Extravagant": "Impact-Extravagant",
+        "Interact": "Interact",
+        "Montserrat-Italic-VariableFont_wght": "Montserrat-Italic-VariableFont_wght",
+        "Montserrat-VariableFont_wght": "Montserrat-VariableFont_wght",
+        "Popping-Cute": "Popping-Cute",
+        "Roboto-Bold": "Roboto-Bold",
+        "Roboto-Regular": "Roboto-Regular",
+        "San Antonio Charros_personal_use_only": "San Antonio Charros_personal_use_only"
+    }
+    return font_map.get(font_choice, "Impact")
+
 def get_pro_font(font_choice, font_size):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     FONT_DIR = os.path.join(BASE_DIR, "fonts")
     
-    choice_lower = font_choice.lower()
-    custom_font_file = None
+    custom_font_file = font_choice if font_choice.endswith(".ttf") else font_choice + ".ttf"
+    custom_path = os.path.join(FONT_DIR, custom_font_file)
     
-    # 15o fonts ki proper mapping screenshot ke mutabiq
-    if "antonio zull" in choice_lower:
-        custom_font_file = "AntonioZull-Brush.ttf"
-    elif "bebas neue" in choice_lower:
-        custom_font_file = "BebasNeue-Regular.ttf"
-    elif "impact club" in choice_lower:
-        custom_font_file = "Impact Club.ttf"
-    elif choice_lower == "impact" or "impact standard" in choice_lower:
-        custom_font_file = "Impact.ttf"
-    elif "impact brutas" in choice_lower:
-        custom_font_file = "Impactbrutas.ttf"
-    elif choice_lower == "impacted":
-        custom_font_file = "IMPACTED.ttf"
-    elif "impacted 2.0" in choice_lower:
-        custom_font_file = "Impacted2.0.ttf"
-    elif "impact extravagant" in choice_lower:
-        custom_font_file = "Impact-Extravagant.ttf"
-    elif "interact" in choice_lower:
-        custom_font_file = "Interact.ttf"
-    elif "montserrat italic" in choice_lower:
-        custom_font_file = "Montserrat-Italic-VariableFont_wght.ttf"
-    elif "montserrat" in choice_lower:
-        custom_font_file = "Montserrat-VariableFont_wght.ttf"
-    elif "popping cute" in choice_lower:
-        custom_font_file = "Popping-Cute.ttf"
-    elif "roboto bold" in choice_lower:
-        custom_font_file = "Roboto-Bold.ttf"
-    elif "roboto" in choice_lower:
-        custom_font_file = "Roboto-Regular.ttf"
-    elif "san antonio charros" in choice_lower:
-        custom_font_file = "San Antonio Charros_personal_use_only.ttf"
-        
-    if custom_font_file:
-        custom_path = os.path.join(FONT_DIR, custom_font_file)
-        if os.path.exists(custom_path):
-            try:
-                return ImageFont.truetype(custom_path, int(font_size * 2.2))
-            except Exception:
-                pass
+    if os.path.exists(custom_path):
+        try:
+            return ImageFont.truetype(custom_path, int(font_size * 2.2))
+        except Exception:
+            pass
 
-    # Fallback default font agar koi file missing ho
     fallback_path = os.path.join(FONT_DIR, "Impact.ttf")
     if not os.path.exists(fallback_path):
         fallback_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
